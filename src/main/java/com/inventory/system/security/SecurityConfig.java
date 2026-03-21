6382c7a3c5ced2c0f82bd2eb2d00749f0244caaa
saimanojburre <saimanoj.burre@gmail.com>
2026-03-21 23:13:54 +0530

    new cors added

--- src/main/java/com/inventory/system/security/SecurityConfig.java
+++ src/main/java/com/inventory/system/security/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
 
 		CorsConfiguration configuration = new CorsConfiguration();
 
-		configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+		configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://inventory.theairawatkitchen.in"));
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(List.of("*"));
 		configuration.setAllowCredentials(true);
