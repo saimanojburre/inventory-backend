6b0a2b16799b800c0fbbf332c408e6cb084b8ab3
saimanojburre <saimanoj.burre@gmail.com>
2026-05-17 15:08:57 +0530

    timezone added

--- src/main/java/com/inventory/system/InventorySystemApplication.java
+++ src/main/java/com/inventory/system/InventorySystemApplication.java
@@ -1,7 +1,10 @@
 package com.inventory.system;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class InventorySystemApplication {
@@ -9,5 +12,10 @@ public class InventorySystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventorySystemApplication.class, args);
 	}
-
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(
+                TimeZone.getTimeZone("Asia/Kolkata")
+        );
+    }
 }
